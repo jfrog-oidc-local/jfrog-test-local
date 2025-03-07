6ac432e0f201be0643b8f3ae77862b9e504df43f
MoturiBabu <moturibvr789@gmail.com>
2025-03-07 13:50:20 +0530

    Update _terraform.tf

--- _terraform.tf
+++ _terraform.tf
@@ -9,9 +9,9 @@ terraform {
       version = "6.2.3"
     }
   }
-  backend "s3" {
-    bucket = "edvops-404188485533-us-east-1-tfstate-management-bucket"
-    key    = "gtio-cloud-ops-jfrog-oidc-test/jfrog-oidc-terraform.tfstate"
-    region = "us-east-1"
-  }
+ # backend "s3" {
+   # bucket = "edvops-404188485533-us-east-1-tfstate-management-bucket"
+   # key    = "gtio-cloud-ops-jfrog-oidc-test/jfrog-oidc-terraform.tfstate"
+   # region = "us-east-1"
+ # }
 }
