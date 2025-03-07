d54edbeb437b7f22b4d161b6d058dd55b8cb59d5
MoturiBabu <moturibvr789@gmail.com>
2025-03-07 14:14:24 +0530

    Update terraform.tf

--- modules/terraform.tf
+++ modules/terraform.tf
@@ -1,9 +1,9 @@
 terraform {
   required_providers {
-    artifactory = {
-      source  = "jfrog/artifactory"
-      version = "12.7.1"
-    }
+   # artifactory = {
+     # source  = "jfrog/artifactory"
+     # version = "12.7.1"
+   # }
     platform = {
       source  = "jfrog/platform"
       version = "2.2.1"
